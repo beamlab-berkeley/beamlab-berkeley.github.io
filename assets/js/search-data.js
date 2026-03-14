b8288ea089b5fb51ad69c08da083f698275d615e
AdityaMuppala <142175661+AdityaMuppala@users.noreply.github.com>
2026-03-14 01:26:12 +0000

    Deploying to gh-pages from @ beamlab-berkeley/beamlab-berkeley.github.io@6838f4a09c95eb814b5b12a34fe697abf093d78b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-home",
-          title: "Home",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "dropdown-current-members",
+  },{id: "dropdown-current-members",
               title: "Current Members",
               description: "",
               section: "Dropdown",

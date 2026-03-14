1b021aa30b7ade378a3f52c1ec6567745edd6ef9
AdityaMuppala <142175661+AdityaMuppala@users.noreply.github.com>
2026-03-14 00:46:10 +0000

    Deploying to gh-pages from @ beamlab-berkeley/beamlab-berkeley.github.io@cdabeff81d55c038be75d8979d8e952cc626e26b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,14 +58,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/publications/#conference";
               },
-            },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "dropdown-ee-105",
+            },{id: "dropdown-ee-105",
               title: "EE 105",
               description: "",
               section: "Dropdown",
